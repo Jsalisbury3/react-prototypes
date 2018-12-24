50cb7267a0a8ddfc89bad9f8b174b428afed92ff
Jordan Salisbury <salisburyjordan21@gmail.com>
2018-12-24 09:09:02 -0800

    Progress

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Welcome from 'welcome';
 
+
 export default () => {
     return(
         <div className="container">
-        <h1>Whats up</h1>
-        <Route path='/' component = {Welcome}/>
+            <h1>Whats up</h1>
+            <Route path="/" component={Welcome}/>
         </div>
     )
 };
