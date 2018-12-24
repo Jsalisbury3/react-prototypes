fb6cc7c7dcc515eaba20b11537f852e09a060f5f
Jordan Salisbury <salisburyjordan21@gmail.com>
2018-12-24 12:26:26 -0800

    progress on proto

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Welcome from 'welcome';
+import Welcome from './welcome';
+import Nav from './nav';
+import Mac from './our_macarons';
 
 
 export default () => {
     return(
         <div className="container">
-            <h1>Whats up</h1>
-            <Route path="/" component={Welcome}/>
+            <Nav/>
+            <Route exact path="/" component={Welcome}/>
+            <Route path="/our_macarons" component={Mac}/>
         </div>
     )
 };
---
+++ src/components/nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom'; 
+
+export default props => (
+    <ul className="nav nav-tabs mt-3">
+        <li className="nav-item"> 
+            <Link to='/' className='nav-link'>Welcome</Link>
+        </li>
+        <li className="nav-item">
+            <Link to="/our_macarons" className="nav-link">Macarons</Link>
+        </li>
+    </ul>
+)
